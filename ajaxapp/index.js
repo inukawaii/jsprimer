6af01dea6b3539f84e0683b4453cd9b58f26abea
mayumi aka inukawaii <inukawaii999@gmail.com>
2021-02-19 00:02:41 +0900

    レスポンスの受け取り

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -1,1 +1,9 @@
 console.log("index.js: loaded");
+const userId = 'js-primer-example';
+fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+  .then((response) => {
+    console.log(response.status);
+    return response.json().then((userInfo) => {
+      console.log(userInfo);
+    })
+  })
