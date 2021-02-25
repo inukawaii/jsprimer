3be26f0dd42cfafdb1d99e29809b7a5646284459
mayumi aka inukawaii <inukawaii999@gmail.com>
2021-02-26 00:39:45 +0900

    関数の分割

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -1,6 +1,10 @@
 console.log("index.js: loaded");
 const userId = 'js-primer-example';
 
+function main() {
+  fetchUserInfo('js-primer-example');
+}
+
 function fetchUserInfo(userId) {
   fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
     .then((response) => {
@@ -9,24 +13,32 @@ function fetchUserInfo(userId) {
         console.error('エラーレスポンス', response);
       } else {
         return response.json().then((userInfo) => {
-          const view = escapeHTML`
-          <h4>${userInfo.name} (@${userInfo.login})</h4>
-          <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-          <dl>
-              <dt>Location</dt>
-              <dd>${userInfo.location}</dd>
-              <dt>Repositories</dt>
-              <dd>${userInfo.public_repos}</dd>
-          </dl>
-          `;
-          const result = document.getElementById('result');
-          result.innerHTML = view;          
+          const view = createView(userInfo)
+          displayView(view);
         })
       }
     })
     .catch((error) => {
       console.log(error);
     })
+}
+
+function createView(userInfo) {
+  return escapeHTML`
+  <h4>${userInfo.name} (@${userInfo.login})</h4>
+  <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+  <dl>
+      <dt>Location</dt>
+      <dd>${userInfo.location}</dd>
+      <dt>Repositories</dt>
+      <dd>${userInfo.public_repos}</dd>
+  </dl>
+  `;
+}
+
+function displayView(view) {
+  const result = document.getElementById('result');
+  result.innerHTML = view;
 }
 
 function escapeSpecialChars(str) {
