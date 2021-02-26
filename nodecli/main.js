0a7745985f98937296fe4efef8d1299cca746866
mayumi aka inukawaii <inukawaii999@gmail.com>
2021-02-26 19:03:45 +0900

    markedパッケージを使う

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,6 +1,7 @@
 const myModule = require("./my-module");
 const program = require("commander");
 const fs = require('fs');
+const marked = require('marked');
 
 program.parse(process.argv);
 const filePath = program.args[0];
@@ -11,5 +12,6 @@ fs.readFile(filePath, { encoding: 'utf8' },(err, file) => {
     process.exit(1);
     return;
   }
-  console.log(file);
+  const html = marked(file);
+  console.log(html);
 })
