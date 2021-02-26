7c274e623dd06d23988e674562a9d10df3e105b4
mayumi aka inukawaii <inukawaii999@gmail.com>
2021-02-26 18:46:11 +0900

    readFile関数を使う

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,5 +1,10 @@
 const myModule = require("./my-module");
 const program = require("commander");
+const fs = require('fs');
+
 program.parse(process.argv);
 const filePath = program.args[0];
-console.log(filePath);
+
+fs.readFile(filePath, { encoding: 'utf8' },(err, file) => {
+  console.log(file);
+})
