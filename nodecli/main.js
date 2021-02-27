250ddfc089d6339bd24a2d4eba7ff17cdf7ca858
mayumi aka inukawaii <inukawaii999@gmail.com>
2021-02-27 09:38:33 +0900

    不要なファイル削除 アプリケーションをモジュールに分割する

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,7 +1,6 @@
-const myModule = require("./my-module");
 const program = require("commander");
 const fs = require('fs');
-const marked = require('marked');
+const md2html = require('./md2html');
 
 program.option('--gfm', 'GFMを有効にする');
 program.parse(process.argv);
@@ -18,8 +17,6 @@ fs.readFile(filePath, { encoding: 'utf8' },(err, file) => {
     process.exit(1);
     return;
   }
-  const html = marked(file, {
-    gfm: cliOptions.gfm,
-  });
+  const html = md2html(file, cliOptions)
   console.log(html);
 })
