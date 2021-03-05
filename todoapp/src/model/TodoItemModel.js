9c3211e3cb3cfc70ebcfb4846724644127dbec46
mayumi aka inukawaii <inukawaii999@gmail.com>
2021-03-05 19:23:43 +0900

    TodoListModelに指定したTodoアイテムの更新処理を追加する

--- todoapp/src/model/TodoItemModel.js
+++ todoapp/src/model/TodoItemModel.js
@@ -6,4 +6,12 @@ export class TodoItemModel {
     this.title = title;
     this.completed = completed;
   }
+  updateTodo({ id, completed }) {
+    const todoItem = this.items.find(todo => todo.id === id);
+    if (!todoItem) {
+      return;
+    }
+    todoItem.completed = completed;
+    this.emitChange();
+  }
 }
